04e9c50fce9fb923b6774fe9d0510199591bf702
Cora Wong <cora@Coras-MBP.hitronhub.home>
2018-06-14 12:12:10 -0700

    Modified css

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,7 +4,8 @@ class Modal extends Component {
   render() {
     return (
       <div className="Modal">
-        <img src={this.props.modalItem.thumbnailUrl} alt={this.props.modalItem.title} />
+        <img src={this.props.modalItem.url} alt={this.props.modalItem.title} />
+        <p>{this.props.modalItem.title}</p>
       </div>
     );
   }
